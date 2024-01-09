14d5e719213c1a156e2f329bd63cf0c1c7e11ea6
kouss <koceila.habarek94@gmail.com>
2024-01-09 22:55:27 +0100

    navBar changed

--- JS/sidenav.js
+++ JS/sidenav.js
@@ -1,50 +1,37 @@
 /* Sidenav */
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
     }
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
     }
+
     
     document.getElementById('openBtn').addEventListener('click', openNav);
     document.getElementById('mySidenav').addEventListener('click', closeNav);
     
     document.addEventListener('click', function(event) {
-         document.getElementById("mySidenav");
-         document.getElementById("openBtn");
+        const mySidenav = document.getElementById("mySidenav");
+        const openBtn = document.getElementById("openBtn");
 
         if (event.target !== mySidenav && event.target !== openBtn && !mySidenav.contains(event.target)) {
             closeNav();
         }
     });
 });
-    
-
-
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to open the side navigation menu
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-    }
-  
-    // Function to close the side navigation menu
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-    }
-  
-    // Event listener for the "open" button
-    document.getElementById("openBtn").addEventListener('click', openNav);
-  
-    // Event listener for clicks outside the side navigation menu
-    document.addEventListener('click', function(event) {
-      var sidenav = document.getElementById("mySidenav");
-      var openBtn = document.getElementById("openBtn");
-  
-      if (event.target !== sidenav && event.target !== openBtn && !sidenav.contains(event.target)) {
-        closeNav();
-      }
-    });
-  });
 */
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sideNav = document.querySelector(".sideNav");
+    const openNav = document.querySelector(".openNav");
+
+    openNav.addEventListener("click", function() {
+        if (window.getComputedStyle(sideNav).display === "none") {
+            sideNav.style.display = "flex";
+        }
+        else {
+            sideNav.style.display = "none";
+        }
+    })
+});
